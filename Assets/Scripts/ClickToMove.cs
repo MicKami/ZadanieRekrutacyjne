e2503bae4862afba3f925e88d740b8201d84bc6b
mich.kam97@gmail.com <mich.kam97@gmail.com>
2024-01-10 23:38:55 +0100

    Add ClickToMove script which sends movement commands to characters read from CharacterSelectionData

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class ClickToMove : MonoBehaviour
+{
+	[SerializeField] private CharacterSelectionData selectionData;
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (TryGetFloorPoint(out var point))
+			{
+				foreach (var character in selectionData.AllCharacters)
+				{
+					if (selectionData.Selected == character)
+					{
+						character.MoveTo(point);
+					}
+					else
+					{
+						character.Follow(selectionData.Selected);
+					}
+				} 
+			}
+		}
+	}
+
+	private bool TryGetFloorPoint(out Vector3 point)
+	{
+		point = Vector3.zero;
+		Plane plane = new Plane(Vector3.up, Vector3.zero);
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (plane.Raycast(ray, out var distance))
+		{
+			point = ray.GetPoint(distance);
+			return true;
+		}
+		return false;
+	}
+}
