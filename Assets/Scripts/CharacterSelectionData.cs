51c2732c56fcdf8ad43abe190cce2f20791f3541
mich.kam97@gmail.com <mich.kam97@gmail.com>
2024-01-12 16:36:29 +0100

    Make CharacterSelectionData scriptable object reset in the editor after exiting play mode

--- Assets/Scripts/CharacterSelectionData.cs
+++ Assets/Scripts/CharacterSelectionData.cs
@@ -6,4 +6,27 @@ public class CharacterSelectionData : ScriptableObject
 {
 	public List<Character> AllCharacters { get; set; } = new();
 	public Character Selected { get; set; }
+
+#if UNITY_EDITOR
+	//Reset SO state after exiting playmode
+	private void EditorApplication_playModeStateChanged(UnityEditor.PlayModeStateChange state)
+	{
+		if (state == UnityEditor.PlayModeStateChange.ExitingPlayMode)
+		{
+			AllCharacters.Clear();
+			Selected = null;
+		}
+	}
+	private void OnEnable()
+	{
+		UnityEditor.EditorApplication.playModeStateChanged += EditorApplication_playModeStateChanged;
+	}
+
+	private void OnDisable()
+	{
+		UnityEditor.EditorApplication.playModeStateChanged -= EditorApplication_playModeStateChanged;
+	}
+
+#endif
+
 }
