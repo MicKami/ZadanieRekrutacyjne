568945c6ab10add98c5539a1967f506f3a965280
mich.kam97@gmail.com <mich.kam97@gmail.com>
2024-01-13 15:09:05 +0100

    Add GUID reference to a SaveableComponent

--- Assets/Scripts/SaveSystem/SaveableComponent.cs
+++ Assets/Scripts/SaveSystem/SaveableComponent.cs
@@ -1,10 +1,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using MicKami.PolymorphicSerialization;
+using System;
 
 [RequireComponent(typeof(SerializedGUIDComponent))]
 public class SaveableComponent : MonoBehaviour
 {
     [SerializeReference, Polymorphic]
     private List<ISaveable> saveables = new();
+
+
+	private SerializedGUIDComponent guid;
+	public Guid Id
+	{
+		get
+		{
+			guid ??= GetComponent<SerializedGUIDComponent>();
+			return guid.Id;
+		}
+	}
+
 }
