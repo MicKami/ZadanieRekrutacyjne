5e2061374195691ef67ba07b948283e200b1013c
mich.kam97@gmail.com <mich.kam97@gmail.com>
2024-01-13 14:46:24 +0100

    Make the GUID required for the SaveableComponent

--- Assets/Scripts/SaveSystem/SaveableComponent.cs
+++ Assets/Scripts/SaveSystem/SaveableComponent.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using MicKami.PolymorphicSerialization;
+
+[RequireComponent(typeof(SerializedGUIDComponent))]
 public class SaveableComponent : MonoBehaviour
 {
     [SerializeReference, Polymorphic]
