bbd28946eea6f54f46f7c60bbb0f360108cb8df3
mich.kam97@gmail.com <mich.kam97@gmail.com>
2024-01-15 21:55:39 +0100

    Add json friendly vector4 type

--- Assets/Scripts/SaveSystem/JsonSerializedTypes.cs
+++ Assets/Scripts/SaveSystem/JsonSerializedTypes.cs
@@ -16,4 +16,20 @@ public static class JsonSerializedTypes
 		public static implicit operator Vector3(Vector3Json v) => new(v.x, v.y, v.z);
 		public static implicit operator Vector3Json(Vector3 v) => new(v.x, v.y, v.z);
 	}
+
+	public struct Vector4Json
+	{
+		public float x, y, z, w;
+
+		public Vector4Json(float x, float y, float z, float w)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.w = w;
+		}
+
+		public static implicit operator Quaternion(Vector4Json v) => new(v.x, v.y, v.z, v.w);
+		public static implicit operator Vector4Json(Quaternion v) => new(v.x, v.y, v.z, v.w);
+	}
 }
